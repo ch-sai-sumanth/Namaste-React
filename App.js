2375554e40767a8361734e615e963dac26187c9e
ch-sai-sumanth <saisumanth0531@gmail.com>
2024-01-16 16:47:30 +0530

    day-3

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent=React.createElement("div",{id:"parent"}, 
 [React.createElement("h1",{id:"child"},"I am h1"),React.createElement("h2",{id:"child"},"I am h2")]);
