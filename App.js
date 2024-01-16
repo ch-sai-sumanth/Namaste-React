644f346d005001d8d0d7f48f6b183bac0bf14507
ch-sai-sumanth <saisumanth0531@gmail.com>
2024-01-16 18:24:51 +0530

    jsx element is not HTML code written in JS

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent=React.createElement("div",{id:"parent"}, 
-[React.createElement("h1",{id:"child"},"I am h1"),React.createElement("h2",{id:"child"},"I am h2")]);
+// const parent=React.createElement("div",{id:"parent"}, 
+// [React.createElement("h1",{id:"child"},"I am h1"),React.createElement("h2",{id:"child"},"I am h2")]);
 
-const heading=React.createElement("h1",{id:"heading"},"Hello World From React!");
+// const heading=React.createElement("h1",{id:"heading"},"Hello World From React!");
 
+const jsxElement= (<h1 id="heading">Namaste React from JSX</h1>);
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(jsxElement);
 
-console.log(parent);
+console.log(jsxElement);
