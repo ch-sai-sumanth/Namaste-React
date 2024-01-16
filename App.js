b8f31ff4b966d708bd5a73e4424149afe3272f62
ch-sai-sumanth <saisumanth0531@gmail.com>
2024-01-16 18:35:32 +0530

    component composition

--- App.js
+++ App.js
@@ -6,9 +6,16 @@ import ReactDOM from "react-dom/client";
 
 // const heading=React.createElement("h1",{id:"heading"},"Hello World From React!");
 
-const jsxElement= (<h1 id="heading">Namaste React from JSX</h1>);
+const Title=()=>(<h1>Namaste React from Title functional component</h1>);
+
+const Head=()=>(
+    <>
+    {/* component composition: using a component in other component */}
+    <Title/>   
+    <h2>Namsaste React from Head Component</h2>
+    </>
+);
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxElement);
+root.render(<Head/>);
 
-console.log(jsxElement);
